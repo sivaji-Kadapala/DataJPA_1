f4c7fab45e36e883e07843829e5c2e5121aa5e34
sivaji-kadapala <sivajisivaji68@gmail.com>
2025-05-28 22:33:20 +0530

    crud,pagination,jpa repositories

--- src/main/java/com/example/DataJPA_1/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DataJPA_1.repository;
-
-import com.example.DataJPA_1.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-import java.lang.invoke.SerializedLambda;
-
-public interface ProductRepository extends CrudRepository<Product, SerializedLambda> {
-}
